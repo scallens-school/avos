17820ce24bb003090b298a18d67d7189b9745ea1
scallens-school <103920822+scallens-school@users.noreply.github.com>
2022-04-18 09:39:36 +0200

    Add files via upload

---
+++ 01Buur.py
@@ -0,0 +1,7 @@
+naam = input("Hoe heet jij?")
+buur = input("Hoe heet jouw buurman/-vrouw?")
+klas = input("In welke klas zit jij?")
+
+
+print("Ik ben " + naam + ".")
+print("Samen met " + buur + " zit ik in " + klas + ".")
