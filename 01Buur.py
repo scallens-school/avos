18efd5abaf274430df2076c62821107e2b790eab
scallens-school <103920822+scallens-school@users.noreply.github.com>
2022-04-18 09:50:22 +0200

    Delete 01Buur.py

--- 01Buur.py
+++
@@ -1,7 +0,0 @@
-naam = input("Hoe heet jij?")
-buur = input("Hoe heet jouw buurman/-vrouw?")
-klas = input("In welke klas zit jij?")
-
-
-print("Ik ben " + naam + ".")
-print("Samen met " + buur + " zit ik in " + klas + ".")
