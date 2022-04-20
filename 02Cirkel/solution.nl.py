9bbd5adfca6bb176074cb27487ed6c91489c687a
scallens-school <103920822+scallens-school@users.noreply.github.com>
2022-04-20 21:19:29 +0200

    Add files via upload

---
+++ 02Cirkel/solution.nl.py
@@ -0,0 +1,9 @@
+input = input("Wat is de straal van de cirkel?")
+r = float(input)
+d = r*2
+o = d*3.14
+a = r**2 * 3.14
+
+print("De cirkel met straal " + str(r) + " heeft een diameter van " + str(d) + ".")
+print("De cirkel met straal " + str(r) + " heeft een omtrek van " + str(o) + ".")
+print("De cirkel met straal " + str(r) + " heeft een oppervlakte van " + str(a) + ".")
